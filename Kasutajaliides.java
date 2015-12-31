81f64fea0a133a7f1b5509333bfeda74aec100ee
ANDRES KOSTIV <andres.kostiv@gmail.com>
2015-12-31 13:26:25 +0200

    HTML link aktiveeritud

--- Kasutajaliides.java
+++ Kasutajaliides.java
@@ -7,6 +7,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -67,8 +68,10 @@ public class Kasutajaliides extends Application{
         //Nupp "Paku Lehti" ja all tekst URL
         Button pakuLehtiBtn = new Button ("Paku lehti");
         gridAlgus.add(pakuLehtiBtn, 1,6);
-        final Text pakuURL = new Text();
+
+        final Hyperlink pakuURL = new Hyperlink();
         gridAlgus.add(pakuURL, 1, 5, 2, 1);
+
         pakuLehtiBtn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
 
@@ -85,8 +88,19 @@ public class Kasutajaliides extends Application{
                     while ( rs.next() ) {
                         String url = rs.getString("url");
 
-                        juhuslikUrl = "Vaata näiteks seda lehte: " + url;
+                        juhuslikUrl = url;
                         pakuURL.setText(juhuslikUrl);
+
+                        pakuURL.setOnAction(new EventHandler<ActionEvent>() {
+
+                            @Override
+                            public void handle(ActionEvent t) {
+                                getHostServices().showDocument(url);
+                            }
+                        });
+
+                        pakuLehtiBtn.setText("Paku veel lehti");
+
                     }
                     rs.close();
                     stmt.close();
